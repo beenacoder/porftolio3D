ee856a9350dece1add8b5437d2e7c02ac5ba6398
Ariel <cervanteslibch@gmail.com>
2023-08-08 20:58:19 -0300

    adding 3d

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,12 +1,21 @@
+import { Canvas } from "@react-three/fiber"
+import './test.scss';
+import { OrbitControls } from "@react-three/drei";
+
 
 
 const Test = () => {
-  return (
-    <div className='container'>
-        
+    return (
+        <div className='container'>
+            <Canvas>
+                <OrbitControls enableZoom={false} autoRotate />
+                <mesh>
+                    <boxGeometry args= {[1,1,1]} />
+                </mesh>
+            </Canvas>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Test
